ad0b158e719f0bad1f2474f81db93ad17cc28797
Netsanet-dev <Netsanetsolomon41@gmail.com>
2025-04-01 01:08:22 +0300

    feat: Update admin list_display to use 'id' for models and include gender where applicable

--- gamecore/admin.py
+++ gamecore/admin.py
@@ -4,7 +4,7 @@ from .models import League, Season, Club, Player, Coach, Team, Stadium, Referee
 # Register your models here.
 @admin.register(League)
 class LeagueAdmin(admin.ModelAdmin):
-    list_display = ['league_id', 'name', 'country', 'divison', 'created_at', 'updated_at']
+    list_display = ['id', 'name', 'country', 'divison','gender', 'created_at', 'updated_at']
 
 @admin.register(Stadium)
 class StadiumAdmin(admin.ModelAdmin):
@@ -12,25 +12,25 @@ class StadiumAdmin(admin.ModelAdmin):
 
 @admin.register(Season)
 class SeasonAdmin(admin.ModelAdmin):
-    list_display = ['league_id', 'start', 'end', 'current']
+    list_display = ['id', 'league', 'start', 'end', 'current']
 
 @admin.register(Club)
 class ClubAdmin(admin.ModelAdmin):
-    list_display = ['club_id', 'league_id', 'name', 'founded_year']
+    list_display = ['id', 'name', 'founded_year']
 
 @admin.register(Team)
 class TeamAdmin(admin.ModelAdmin):
-    list_display = ['team_id', 'club_id', 'division', 'gender']
+    list_display = ['id', 'club', 'league' ,'division', 'gender']
 
 
 @admin.register(Coach)
 class CoachAdmin(admin.ModelAdmin):
-    list_display = ['coach_id', 'club_id', 'team_id', 'first_name', 'last_name', 'gender', 'age', 'nationality', 'position']
+    list_display = ['id', 'club', 'team', 'first_name', 'last_name', 'gender', 'age', 'nationality', 'position']
 
 
 @admin.register(Player)
 class PlayerAdmin(admin.ModelAdmin):
-    list_display = ['player_id', 'team_id', 'first_name', 'last_name', 'gender', 'age', 'nationality', 'dominant_foot', 'positions']
+    list_display = ['id', 'team', 'first_name', 'last_name', 'gender', 'age', 'nationality', 'dominant_foot', 'positions']
 
 
 @admin.register(Referee)
