c7c0555ed3200730640cd2372b1eb371c7e6bc70
Netsanet-dev <Netsanetsolomon41@gmail.com>
2025-04-01 01:08:56 +0300

    feat: Refactor models to use 'id' for primary keys and update foreign key relationships; include gender field in League, Team, Coach, and Player models

--- gamecore/models.py
+++ gamecore/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from datetime import date
-from django.core.exceptions import ValidationError
 from django.core.validators import MinValueValidator, MaxValueValidator
 from .choices import (
     Gender,
@@ -13,17 +12,18 @@ from .choices import (
 )
 
 class League(models.Model):
-    league_id = models.BigAutoField(primary_key=True)
+    id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=200)
     country = models.CharField(max_length=200)
     logo = models.ImageField(upload_to='images/', blank=True, null=True)
     flag = models.ImageField(upload_to='images/', blank=True, null=True)
     divison = models.CharField(max_length=30, choices=LeagueDivisions.choices, default=LeagueDivisions.TOP)
+    gender = models.CharField(max_length=20, choices=Gender.choices, default=Gender.UNSPECIFIED)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"League name: {self.name} Country: {self.country}"
+        return f"{self.gender}-{self.name} {self.divison} Division "
 
 
 class Stadium(models.Model):
@@ -40,61 +40,41 @@ class Stadium(models.Model):
 
 class Season(models.Model):
     id = models.BigAutoField(primary_key=True, editable=False)
-    league_id = models.ForeignKey(League, on_delete=models.CASCADE, related_name='leagues_season')
+    league = models.ForeignKey(League, on_delete=models.CASCADE, related_name='leagues_season')
     start = models.DateField()
     end = models.DateField()
     current = models.BooleanField(default=False)
 
-    def clean(self):
-        # Check if the start year is not greater than end year
-        if self.start >= self.end:
-            raise ValidationError(f"The end date should be greater than start date.")
-
-    def save(self, *args, **kwargs):
-        today = date.today().year
-        
-        # Check if the current season is not greater or leass than this year.
-        if self.start.year <= today <= self.end.year:
-            self.current = True
-        else:
-            self.current = False
-        
-        if self.current:
-            Season.objects.filter(league_id=self.league_id).exclude(id=self.id).update(current=False)
-        
-        self.full_clean()
-        super().save(*args, **kwargs)
-
     def __str__(self):
-        return f'League: {self.league_id.name} From {self.start} to {self.end}'    
+        return f'League: {self.league.name} From {self.start} to {self.end}'    
 
 
 class Club(models.Model):
-    club_id = models.BigAutoField(primary_key=True)
-    league_id = models.ForeignKey(League, on_delete=models.CASCADE, related_name='league_clubs')
+    id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=200)
     stadium = models.ForeignKey(Stadium, on_delete=models.CASCADE, related_name='home_teams')
     logo = models.ImageField(upload_to='images/', null=True, blank=True)
     founded_year = models.IntegerField(validators=[MinValueValidator(1900), MaxValueValidator(date.today().year)], default=1900,)
 
     def __str__(self):
-        return f"Club name: {self.name}"
+        return f"{self.name}"
 
 
 class Team(models.Model):
-    team_id = models.BigAutoField(primary_key=True, editable=False)
-    club_id = models.ForeignKey(Club, on_delete=models.CASCADE, related_name='team_clubs')
+    id = models.BigAutoField(primary_key=True, editable=False)
+    league = models.ForeignKey(League, on_delete=models.CASCADE, related_name='league_clubs')
+    club = models.ForeignKey(Club, on_delete=models.CASCADE, related_name='team_clubs')
     division = models.CharField(max_length=50, choices=TeamDivisons.choices, default=TeamDivisons.FIRST)
     gender = models.CharField(max_length=20, choices=Gender.choices, default=Gender.UNSPECIFIED)
 
     def __str__(self):
-        return f'{self.club_id.name}, {self.division}, {self.gender}' 
+        return f'{self.club.name}, {self.division}, {self.gender}' 
 
 
 class Coach(models.Model):
-    coach_id = models.BigAutoField(primary_key=True, editable=False)
-    club_id = models.ForeignKey(Club, on_delete=models.CASCADE, related_name='coach_clubs')
-    team_id = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='coach_teams')
+    id = models.BigAutoField(primary_key=True, editable=False)
+    club = models.ForeignKey(Club, on_delete=models.CASCADE, related_name='coach_clubs')
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='coach_teams')
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100, null=True, blank=True)
     gender = models.CharField(max_length=20, choices=Gender.choices, default=Gender.UNSPECIFIED)
@@ -103,12 +83,12 @@ class Coach(models.Model):
     position = models.CharField(max_length=100, choices=CoachingPositions.choices, default=CoachingPositions.HEAD)
 
     def __str__(self):
-        return f'{self.first_name} {self.last_name}, Coach at {self.club_id.name}'
+        return f'{self.first_name} {self.last_name}, Coach at {self.club.name}'
 
 
 class Player(models.Model):
-    player_id = models.BigAutoField(primary_key=True, editable=False)
-    team_id = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='players_team')
+    id = models.BigAutoField(primary_key=True, editable=False)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='players_team')
     first_name = models.CharField(max_length=50)
     last_name =models.CharField(max_length=50, blank=True, null=True)
     gender = models.CharField(max_length=20, choices=Gender.choices, default=Gender.UNSPECIFIED)
@@ -118,7 +98,7 @@ class Player(models.Model):
     positions = models.CharField(max_length=50, choices=PlayerPositions.choices, default=PlayerPositions.GK)
 
     def __str__(self):
-        return f'{self.first_name} {self.last_name} played at {self.team_id.club_id.name}'
+        return f'{self.first_name} {self.last_name} played at {self.team.club.name}'
 
 
 class Referee(models.Model):
