3cd1b064d2f5298519e64831686d340aae8f0ab8
Netsanet-dev <Netsanetsolomon41@gmail.com>
2025-04-03 01:08:57 +0300

    feat: Update API URL paths to remove 'api/' prefix and add refresh token endpoint

--- core/urls.py
+++ core/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    
-    path('api/register/', views.register_user, name='register'),
-    path('api/login/', views.login_user, name='login'),
-    path('api/logout/', views.logout_user, name="logout"),
-    path('api/update_info/', views.update_user_info, name="update_info"),    
-    path('api/update_password/', views.update_password, name='update-password'),
-    path('api/protected/', views.protected_view, name="protected"),
+    path('register/', views.register_user, name='register'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name="logout"),
+    path('update_info/', views.update_user_info, name="update_info"),    
+    path('update_password/', views.update_password, name='update-password'),
+    path('protected/', views.protected_view, name="protected"),
+    path('refresh_token/', views.refresh_token_view, name="refresh-token"),
 ]
