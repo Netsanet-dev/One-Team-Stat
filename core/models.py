e6397133b33bec1da4419e8b4d101d64a45d0831
Netsanet-dev <Netsanetsolomon41@gmail.com>
2025-04-03 01:07:59 +0300

    feat: Refactor user role management to use TextChoices for improved clarity and maintainability

--- core/models.py
+++ core/models.py
@@ -1,17 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-USER_ROLE_ADMIN = 'admin'
-USER_ROLE_PLAYER = 'player'
-USER_ROLE_COACH = 'coach'
-USER_ROLE_USER = 'user'
-
-ROLE_CHOICES = (
-        (USER_ROLE_ADMIN, 'Administrator'),
-        (USER_ROLE_PLAYER, 'Player'),
-        (USER_ROLE_COACH, 'Coach'),
-        (USER_ROLE_USER, 'Regular User')
-    )
+class UserRole(models.TextChoices):
+    LEAGUE_ADMIN = 'league','League Admin'
+    CLUB_ADMIN = 'club ', 'club Admin'
+    TEAM_ADMIN = 'team','Team Admin'
+    USER = 'user', 'Regular User'
 
 class MyUser(AbstractUser):
-   role = models.CharField(max_length=50, choices=ROLE_CHOICES, default='user')
+   role = models.CharField(max_length=50, choices=UserRole.choices, default=UserRole.USER)
