00c0e6bb45befb843b96c3fc28eb019725e25625
Netsanet-dev <Netsanetsolomon41@gmail.com>
2025-04-03 23:38:06 +0300

    feat: Add initial GameStat serializers and URLs; update views with model references

--- gamestat/views.py
+++ gamestat/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+# ALL MODELS 
+# League, League, Stadium, Season, Club, Team, Coach, Player, Referee
+# Fixtures, LineupPlayer, Lineups, substitutions, gamestat, gameevents
+
+# A user want to see
