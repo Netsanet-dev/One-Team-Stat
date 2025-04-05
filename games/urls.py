f6f39089f9df90af1431008c9e03f29b896f62ed
Netsanet-dev <Netsanetsolomon41@gmail.com>
2025-04-05 16:21:24 +0300

    feat: Register LineupPlayerViewSet in the router and update urlpatterns for cleaner API structure

--- games/urls.py
+++ games/urls.py
@@ -6,12 +6,14 @@ router = DefaultRouter()
 
 router.register(r'fixtures', views.FixtureViewSet)
 router.register(r'lineups', views.LineupViewSet)
+router.register(r'lineup_players', views.LineupPlayerViewSet)
 router.register(r'susbstitution', views.SubstitutionViewSet)
 router.register(r'match_officals', views.MatchOfficalViewSet)
 router.register(r'game_events', views.GameEventViewSet)
 router.register(r'match_stats', views.MatchStatViewSet)
 
 
+
 urlpatterns =[
-    path('api/games/', include(router.urls))
+    path('', include(router.urls))
 ]
