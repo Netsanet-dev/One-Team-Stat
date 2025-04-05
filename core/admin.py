66fc8381807d87ebeca6b904003c796b8e8aa2e2
Netsanet-dev <Netsanetsolomon41@gmail.com>
2025-04-05 16:18:11 +0300

    feat: Enhance MyUserAdmin to include role management and improve search/filter capabilities

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import MyUser
 
 # Register your models here.
 @admin.register(MyUser)
-class MyUserAdmin(admin.ModelAdmin):
+class MyUserAdmin(UserAdmin):
     list_display = ['username', 'email', 'first_name', 'last_name', 'role']
-
+    fieldsets = UserAdmin.fieldsets + (
+    ('Role Info', {'fields': ('role',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+    ('Role Info', {'fields': ('role',)}),
+    )
+    search_fields = ('username', 'email', 'role')
+    list_filter = ('role', 'is_staff', 'is_active')
